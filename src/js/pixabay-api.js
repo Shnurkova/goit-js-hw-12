5dfaaeb12b9edebef8c9aea4ca7ba6c9dd15d0f6
Shnurkova <Annshnur@gmail.com>
2024-03-31 19:50:17 +0300

    add

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,21 +4,22 @@ import axios from "axios";
 export let limit = 15;
 
 export async function fetchPhotoFromPixabay() {
-  let inputValueForForm;
+    let inputValueForForm;
     if (page === 1) {
         const inputSearch = form.elements.search;
         inputValueForForm = inputSearch.value.trim().split(' ').join('+');
     } else {
         inputValueForForm = input.trim().split(' ').join('+');
     }
-  
-  const searchParams = new URLSearchParams({
-    key: '43014470-46919859f304ff208235eaa47',
-    q: searchImgs,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-  });
-  const response = await axios.get(`https://pixabay.com/api/?${searchParams}`);
+    const searchParams = new URLSearchParams({
+        key: "42633759-4a44573e34755b4488adb4c1b",
+        q: [inputValueForForm],
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+        page: [page],
+        per_page: [limit]
+    });
+    const response = await axios.get(`https://pixabay.com/api/?${searchParams}`);
     return response.data;
 }
